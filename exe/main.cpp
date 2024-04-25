1bf896184afc48b29dc7a9e2ed070dfaaeb0e651
André Willyan <andrewillyan@tutanota.com>
2024-04-24 23:07:58 -0300

    feat: add example of RegStream as the main exe

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,4 +1,22 @@
+#include "hashi/include/stream.hpp"
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
 
-// NOTE: Dummy binary
-int main() { std::cout << "Hello!" << std::endl; }
+// NOTE: RegStream example
+int main() {
+  std::string file_path = "../db/table.csv";
+  auto fs = std::ifstream(file_path);
+  if (fs.fail()) {
+    throw std::runtime_error(
+        "Failed to open file stream, does the file exist?");
+  }
+
+  auto rs = RegStream(fs);
+  Reg r;
+  std::cout << "DATA on " << file_path << std::endl;
+  while (!rs.endOfStream()) {
+    rs >> r;
+    std::cout << r << std::endl;
+  }
+}
